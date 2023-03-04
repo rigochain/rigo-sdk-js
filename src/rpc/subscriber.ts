a36d45b943f99d473a63d02f36713e1c5c0a8378
kysee <kysee.is@gmail.com>
2023-03-04 14:18:14 +0900

    feat: Fix bug not found WebSocket

--- src/rpc/subscriber.ts
+++ src/rpc/subscriber.ts
@@ -1,47 +1,43 @@
 import {createJSONRPCRequest} from "json-rpc-2.0";
+import WebSocket from 'ws'
 
 export default class Subscriber {
-    #ws: WebSocket
+    #wsconn: WebSocket
     #query: string
 
     constructor(public url:string) {}
 
     start(query: string, cbFunc: (resp:string)=>void) {
-        if(this.#ws) {
+        if(this.#wsconn) {
             console.error("already start.")
         }
         this.#query = query
 
-        this.#ws = new WebSocket(this.url)
-        this.#ws.addEventListener('open', (evt) => {
-            // console.log("this", this)
+        this.#wsconn = new WebSocket(this.url)
+        this.#wsconn.on('error', (err) => {
+            console.error('websocket error:', err)
+            this.stop()
+        })
+        this.#wsconn.on('ping', console.log)
+        this.#wsconn.on('open', () => {
             console.log("websocket open:", this.url)
-            console.log("query         :", this.#query)
             const ret = createJSONRPCRequest("dontcare", 'subscribe', {query: this.#query})
 
             const reqstr = JSON.stringify(ret)
             console.log("websocket request:", reqstr)
-            this.#ws.send(reqstr)
+            this.#wsconn.send(reqstr)
         })
-        this.#ws.addEventListener('error', (evt) => {
-            console.error('websocket error:', evt)
-            this.stop()
-        })
-        this.#ws.addEventListener('message', (evt) => {
-            //console.log('websocket message:', typeof evt.data)
-            let resp = evt.data
-            if(typeof evt.data === 'string') {
-                resp = JSON.parse(evt.data)
-            }
+        this.#wsconn.on('message', (data) => {
+            let resp = JSON.parse(data.toString())
             cbFunc(resp.result)
         })
-        this.#ws.addEventListener('close', (evt) => {
-            console.log('websocket close:', this.#ws.url, evt.code, evt.reason)
+        this.#wsconn.on('close', () => {
+            console.log('websocket closed:', this.url)
         })
     }
     stop() {
-        this.#ws.close(0, "EventListener is closed by application")
-        this.#ws = null
+        this.#wsconn.close(0, "Websocket is closed by application")
+        this.#wsconn = null
     }
 
     getQuery(): string {
