5e15f1a2811d8d419791cc06d584e2dc4a82c9ca
kysee <kysee.is@gmail.com>
2023-03-04 14:40:14 +0900

    feat: Add nodejs explorer sample

--- examples/explorer/scanner.js
+++ examples/explorer/scanner.js
@@ -1,19 +1,6 @@
 import {RWeb3} from 'rigo-sdk-js'
-const rweb3 = new RWeb3("http://192.168.252.60:26657")
+const rweb3 = new RWeb3("http://localhost:26657")
 
-rweb3.subscribe('ws://192.168.252.60:26657/websocket', "tm.event='NewBlockHeader'", (resp) => {
+rweb3.subscribe('ws://localhost:26657/websocket', "tm.event='NewBlockHeader'", (resp) => {
     console.log(resp)
 })
-
-
-
-// import WebSocket from 'ws';
-//
-// const wsconn = new WebSocket("ws://192.168.252.60:26657/websocket")
-// wsconn.on('open', () => {
-//     wsconn.send(`{"jsonrpc":"2.0", "id":"dontcare", "method":"subscribe", "params":{"query":"tm.event='NewBlockHeader'"}}`)
-// })
-//
-// wsconn.on('message', (data) => {
-//     console.log(data.toString())
-// })
