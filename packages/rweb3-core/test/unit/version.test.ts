92080fde64962a5b6851f5272e06d2e25604fac9
Leonard Bae <sungdong1917@regulus.foundation>
2023-07-27 14:19:03 +0900

    2023-07-27  - test develop setting success

---
+++ packages/rweb3-core/test/unit/version.test.ts
@@ -0,0 +1,9 @@
+﻿
+import { RWeb3PkgInfo } from '../../src/version';
+import packageFile from '../../package.json';
+
+describe('rweb3-core package info', () => {
+	it('should RWeb3PkgInfo.version returns the same version set at package.json', () => {
+		expect(packageFile.version).toEqual(RWeb3PkgInfo.version);
+	});
+});
