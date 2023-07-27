c488dcfb0fafca6ecb2d9795564b085a6ec28867
Leonard Bae <sungdong1917@regulus.foundation>
2023-07-27 14:34:12 +0900

    2023-07-27  - first test code

---
+++ packages/rweb3/test/unit/rweb3.test.ts
@@ -0,0 +1,22 @@
+﻿import { RWeb3 } from '../../src';
+import RWeb3Rigo from 'rweb3-rigo';
+
+describe('RWeb3 class', () => {
+
+	it('should initialize RWeb3 instance correctly', () => {
+
+		const provider = 'http://localhost:8545';
+		const rWeb3Instance = new RWeb3(provider);
+
+		// Check if the instance is correctly initialized
+		expect(rWeb3Instance).toBeInstanceOf(RWeb3);
+
+	});
+
+	it('should initialize RWeb3Rigo module correctly', () => {
+		const rWeb3Instance = new RWeb3();
+
+		// Check if the rigo module is correctly initialized
+		expect(rWeb3Instance.rigo).toBeInstanceOf(RWeb3Rigo);
+	});
+});
