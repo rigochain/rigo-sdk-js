6b0ec5c8f17b96d0019c813994b9f7992f253021
Leonard Bae <sungdong1917@regulus.foundation>
2023-09-07 10:24:49 +0900

    2023-09-07  - rigo-contract import

---
+++ packages/rweb3-rigo-contract/src/constants.ts
@@ -0,0 +1,9 @@
+﻿import { AbiEventFragment } from 'rweb3-types';
+
+export const ALL_EVENTS = 'ALLEVENTS';
+export const ALL_EVENTS_ABI = {
+    name: ALL_EVENTS,
+    signature: '',
+    type: 'event',
+    inputs: [],
+} as AbiEventFragment & { signature: string };
