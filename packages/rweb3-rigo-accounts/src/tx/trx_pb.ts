8a9a16ae46a51c768b5c6eb1229c00532f72eebe
EokgiLee <jarry.lee@themedium.io>
2023-09-19 15:05:06 +0900

    feat: modify import protobufjs

--- packages/rweb3-rigo-accounts/src/tx/trx_pb.ts
+++ packages/rweb3-rigo-accounts/src/tx/trx_pb.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import Long from 'long';
-import _m0 from 'protobufjs/minimal';
+import _m0 from 'protobufjs';
 
 import { TrxProto } from '@rigochain/rweb3-types';
 import BN from 'bn.js';
