b15b284107ca367c1214df44a3b0e360ff8e2b04
EokgiLee <jarry.lee@themedium.io>
2023-09-19 16:07:57 +0900

    feat: version update

--- packages/rweb3/src/version.ts
+++ packages/rweb3/src/version.ts
@@ -1,1 +1,1 @@
-/* eslint-disable header/header */ export const RWeb3PkgInfo = { version: '1.0.0-alpha.3' };
+/* eslint-disable header/header */ export const RWeb3PkgInfo = { version: '1.0.0-alpha.4' };
